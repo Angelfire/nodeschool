368c18b4d1a87be84bb28d7a275ca3f2909893f5
Angelfire <andres.paranoid@gmail.com>
2014-03-05 21:34:24 -0500

    less lines of code

--- level1/exercise4.js
+++ level1/exercise4.js
@@ -4,10 +4,9 @@ var filepath = process.argv[2];
 
  // fs.readFile(file, 'utf8', callback) can also be used
 fs.readFile(filepath, function(err, data){
-  if (err){
-    throw err;
-  } else {
-    var linesLength = data.toString().split('\n').length - 1;
-    console.log(linesLength);
-  }
+	var linesLength = data.toString().split('\n').length - 1;
+
+  if (err) throw err;
+
+  console.log(linesLength);
 });
